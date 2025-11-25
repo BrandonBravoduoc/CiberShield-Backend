c08a8184f25bf6249703e73c021f00da120cb4a0
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-25 20:54:26 -0300

    primer commit

---
+++ src/main/java/com/cibershield/cibershield/CibershieldApplication.java
@@ -0,0 +1,13 @@
+package com.cibershield.cibershield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibershieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibershieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibershield/cibershield/CibershieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibershield.cibershield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibershieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
