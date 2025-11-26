1a996ce7335a3f63f24d3c1bdcae53b42b216612
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-25 22:22:26 -0300

    ModelProduct

--- src/main/java/com/cibershield/cibershield/model/product/Product.java
+++ src/main/java/com/cibershield/cibershield/model/product/Product.java
@@ -1,9 +1,14 @@
 package com.cibershield.cibershield.model.product;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,5 +25,25 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "Stock", nullable = false)
+    private Integer Stock;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "Image_url")
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "id_sub_category", nullable = true)
+    private SubCategory subCategory;
+
+    @ManyToOne
+    @JoinColumn(name = "id_trade_mark", nullable = true )
+    private TradeMark tradeMark;
+
+
 }
--- src/main/java/com/cibershield/cibershield/model/product/SubCategory.java
+++ src/main/java/com/cibershield/cibershield/model/product/SubCategory.java
@@ -1,5 +1,27 @@
 package com.cibershield.cibershield.model.product;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name ="sub_category")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubCategory {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String subCategoryName;
+
 }
