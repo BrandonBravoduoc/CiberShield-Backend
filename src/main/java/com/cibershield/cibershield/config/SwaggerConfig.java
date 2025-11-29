78d48113642a7dd539c3ee649495b36001c3bd5c
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-28 21:26:02 -0300

    ola swagger

--- src/main/java/com/cibershield/cibershield/config/SwaggerConfig.java
+++ src/main/java/com/cibershield/cibershield/config/SwaggerConfig.java
@@ -1,22 +1,29 @@
 package com.cibershield.cibershield.config;
 
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(
-                new Info()
-                        .title("API demo")
-                        .version("0.1")
-                        .description("Api de repaso"));
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info().title("CiberShield API").version("1.0"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+            .security(List.of(new SecurityRequirement().addList("bearerAuth")));
     }
-
 }
