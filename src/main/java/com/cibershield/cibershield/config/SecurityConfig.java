f768e8eda0c57d130f0a5e3e3f6efae0b80e6544
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-11-30 17:20:05 -0300

    chaoSeguridadOlaMundo

--- src/main/java/com/cibershield/cibershield/config/SecurityConfig.java
+++ src/main/java/com/cibershield/cibershield/config/SecurityConfig.java
@@ -1,45 +1,13 @@
 package com.cibershield.cibershield.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.cibershield.cibershield.util.JwtFilter;
 
 
 @Configuration
-@EnableMethodSecurity
 public class SecurityConfig {
-
-    @Autowired
-    private JwtFilter jwtFilter;
-
-   @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-
-                .requestMatchers("/api/v1/auth/**").permitAll() 
-
-                .requestMatchers("/doc/**").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/swagger-ui.html").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/swagger-ui-custom.html").permitAll() 
-              
-                .anyRequest().authenticated()                    // el resto sigue protegido con JWT
-            )
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
