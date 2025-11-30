049c9f8f5231ffb92ac5484a7f6202e488f29267
Deybi-hud <hu4nt4r@gmail.com>
2025-11-30 00:31:25 -0300

    Update CategoryResponseDTO

--- src/main/java/com/cibershield/cibershield/model/product/SubCategory.java
+++ src/main/java/com/cibershield/cibershield/model/product/SubCategory.java
@@ -5,13 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name ="sub_category")
+@Table(name = "sub_category")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,5 +25,13 @@ public class SubCategory {
 
     @Column(name = "name", nullable = false, unique = true)
     private String subCategoryName;
+
+    @ManyToOne
+    @JoinColumn(name = "id_product", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "id_category", nullable = false)
+    private Category category;
 
 }
