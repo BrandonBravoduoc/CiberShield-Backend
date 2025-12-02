e8a71abec86f6fb41507dd8066f028e572dba279
Deybi-hud <hu4nt4r@gmail.com>
2025-12-01 21:01:46 -0300

    Update controlador  de productos

--- src/main/java/com/cibershield/cibershield/controller/order/OrderController.java
+++ src/main/java/com/cibershield/cibershield/controller/order/OrderController.java
@@ -1,41 +1,8 @@
 package com.cibershield.cibershield.controller.order;
 
-import java.util.List;
 
-import org.apache.http.HttpStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.cibershield.cibershield.dto.productsDTO.ProductDTO;
-import com.cibershield.cibershield.service.product.ProductService;
-import com.cloudinary.Cloudinary;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-@RestController
-@RequestMapping("/api/v1/products")
 public class OrderController {
 
-    @Autowired
-    private ProductService productService;
 
-    @Autowired
-    private Cloudinary cloudinary;
-
-    @GetMapping
-    public ResponseEntity<List<ProductDTO.Response>> listAll() {
-        return ResponseEntity.ok(productService.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id) {
-        try {
-            return ResponseEntity.ok(productService.findById(id));
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.SC_NOT_FOUND).body(e.getMessage());
-        }
-    }
 
 }
--- src/main/java/com/cibershield/cibershield/controller/product/ProductController.java
+++ src/main/java/com/cibershield/cibershield/controller/product/ProductController.java
@@ -1,5 +1,53 @@
 package com.cibershield.cibershield.controller.product;
 
+import java.util.List;
+
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cibershield.cibershield.dto.productsDTO.ProductDTO;
+import com.cibershield.cibershield.service.product.ProductService;
+import com.cloudinary.Cloudinary;
+
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO.Response>> listAll() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(productService.findById(id));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.SC_NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            productService.deleteProduct(id);
+            return ResponseEntity.noContent().build(); 
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.SC_NOT_FOUND).body(e.getMessage());
+        }
+    }
     
 }
