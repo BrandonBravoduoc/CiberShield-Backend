fc4a840585143bbeb0ced889cd5bf803b728a0b1
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-12-08 21:16:41 -0300

    xdd

--- src/main/java/com/cibershield/cibershield/controller/user/UserController.java
+++ src/main/java/com/cibershield/cibershield/controller/user/UserController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -125,7 +124,7 @@ public class UserController {
         }
     }
 
-    @PutMapping("/update")
+    @PatchMapping("/update")
     public ResponseEntity<?> updateContact(@RequestBody ContactDTO.UpdateContactWithAddress dto) {
         try {
             Long userId = jwtUtil.getCurrentUserId();
